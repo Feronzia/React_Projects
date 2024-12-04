6ea2e3bca9ae135a946da29b48336152c892bde1
Fathima <fathimaferonzia2004@gmail.com>
2024-12-04 13:09:43 +0530

    Initial commit

---
+++ Data.js
@@ -0,0 +1,17 @@
+export const userList = [
+    {
+        name: "fathima feronzia",
+        email: "fathimaferonzias20132@gmail.com",
+        id: 1
+    },
+    {
+        name: "Ragavi",
+        email: "ragavi2003@gmail.com",
+        id: 2
+    },
+    {
+        name: "Monica",
+        email: "moni232@gmail.com",
+        id: 3
+    }
+]
